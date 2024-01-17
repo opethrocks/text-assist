21f2ebca4ef2f494405d36d6b03fa89e2eb2c296
Matt <tav3@tutanota.com>
2024-01-17 16:29:02 -0600

    Remove unnecessary call to compose module

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const receive = require("./receive/receive");
-const compose = require("./send/compose");
 const express = require("express");
 const app = express();
 
 require("dotenv").config();
 
 app.use("/", receive);
-app.use("/", compose);
