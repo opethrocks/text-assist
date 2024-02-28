5e1e36025bf1ff56e21e4c45e941ad63147c815c
Matt <tav3@tutanota.com>
2024-02-28 08:56:09 -0600

    Refactor logic from send & receive into services

--- services/imageGenerator.js
+++ services/imageGenerator.js
@@ -1,10 +1,27 @@
 const OpenAI = require("openai");
+const telnyxSend = require("../services/telnyxSend");
+const completions = require("../services/completions");
+
 
 const openAiApiKey = process.env.OPENAI_API_KEY;
 
 const openai = new OpenAI(openAiApiKey);
 
-const imageGenerator = async (generateImage, formattedMessage) => {
+const imageGenerator = async (messageContent, incomingNumber) => {
+   //Triggers array of words that indicate a user is a requesting an image
+   const triggers = ["image", "photo", "picture", "painting"];
+   const generateImage = triggers.some((item) => messageContent.includes(item));
+ 
+   //Remove punctuation from message content and format string for naming the file for storage
+   const punctuationRegex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+  const formattedMessage = messageContent
+  .replace(punctuationRegex, "")
+
+  .slice(
+    messageContent.indexOf(
+      triggers.find((word) => messageContent.includes(word)),
+    ),
+  );
   //Also check the message type to prevent double uploads to spaces
   //If generateImage is true, call openai images generate.
   if (generateImage) {
@@ -17,9 +34,12 @@ const imageGenerator = async (generateImage, formattedMessage) => {
     });
 
     //Extract image URL from response object
-    url.push(response.data[0].url);
-
-    return url;
+    url.push(await response.data[0].url);
+    //Call telnyx send to deliver the ai response via MMS
+    await telnyxSend(incomingNumber, formattedMessage, url);
+  } else {
+    //If an image is not being requested, call the completions service
+    await completions(messageContent, incomingNumber)
   }
 };
 
--- services/telnyxSend.js
+++ services/telnyxSend.js
@@ -2,51 +2,42 @@ const Telnyx = require("telnyx");
 require("dotenv").config();
 
 const telnyxApiKey = process.env.TELNYX_API_KEY;
-const openAiApiKey = process.env.OPENAI_API_KEY;
 
 const telnyx = Telnyx(telnyxApiKey);
 
 const telnyxSend = async (incomingNumber, messagePayload, url) => {
-  //Retrieve the messaging profile data payload using our Telnyx messaging profile ID.
-  const { data: responseObj } = await telnyx.messagingProfiles.retrieve(
-    process.env.TELNYX_MSG_PROFILE_ID,
-  );
+    
+  try {
+    //Retrieve the messaging profile data payload using our Telnyx messaging profile ID.
+    const { data: responseObj } = await telnyx.messagingProfiles.retrieve(
+      process.env.TELNYX_MSG_PROFILE_ID,
+    );
 
-  /*Destructuring assignment to extract the object containing the phone number from the Telnyx API response.
+    /*Destructuring assignment to extract the object containing the phone number from the Telnyx API response.
       Returns an array of objects containing the phone number associated with the messaging profile.*/
-  let {
-    data: [{ phone_number: telnyxNumber }],
-  } = await responseObj.phone_numbers();
+    let {
+      data: [{ phone_number: telnyxNumber }],
+    } = await responseObj.phone_numbers();
 
-  if (!!url) {
-    //Call Telnyx message creation function if url is provided, pass openai image URL, format text response appropriately
-    telnyx.messages.create(
-      {
-        from: telnyxNumber,
-        to: incomingNumber,
-        text: `Here is your ${messagePayload}`,
-        media_urls: url,
-      },
-      function (err) {
-        if (err) {
-          throw new Error(err);
-        }
-      },
-    );
-  } else {
-    //Telnyx SMS message creation if no media url is provided. The message text will be the AI response found in the competion object.
-    telnyx.messages.create(
-      {
-        from: telnyxNumber,
-        to: incomingNumber,
-        text: messagePayload,
-      },
-      function (err) {
-        if (err) {
-          throw new Error(err);
-        }
-      },
-    );
+    if (incomingNumber !== telnyxNumber) {
+      //Call Telnyx message creation function if url is provided, pass openai image URL, format text response appropriately
+      telnyx.messages.create(
+        {
+          from: telnyxNumber,
+          to: incomingNumber,
+          text: messagePayload,
+          media_urls: url ? url : "",
+        },
+        function (err) {
+          if (err) {
+            throw new Error(err);
+          }
+        },
+      );
+    }
+  } catch (err) {
+    console.log(err);
+    return;
   }
 };
 
