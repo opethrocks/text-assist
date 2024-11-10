4627a2e06df433893567d579dbc2a9b4399daacf
Matt <gatorgoat@protonmail.com>
2024-11-10 08:58:41 -0600

    Fix delivery issues with new ai assistant prompt.

--- services/completions.js
+++ services/completions.js
@@ -9,13 +9,17 @@ const completions = async (messageContent, incomingNumber) => {
   //OpenAI API call. Pass received messageContent to the OpenAI messages array with a user role
   const completion = await openai.chat.completions.create({
     messages: [
-      { role: "system", content: "You are a helpful assistant." },
+      {
+        role: "system",
+        content:
+          "You are a helpful assistant. Please limit your response to 1000 characters or less.",
+      },
       { role: "user", content: messageContent },
     ],
     model: "gpt-3.5-turbo",
   });
   const messagePayload = completion.choices[0].message.content;
-  
+
   //Call telnyx send and pass ai response & incoming phone number
   await send(incomingNumber, messagePayload);
 };
