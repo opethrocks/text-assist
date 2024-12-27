ed7ec937aa224a91a152e4c23a7c94b6a67681bc
Matt <gatorgoat@protonmail.com>
2024-12-26 18:22:33 -0600

    Create s3 presigned url, send back to user.

--- controllers/mediaController.js
+++ controllers/mediaController.js
@@ -1,20 +1,15 @@
 const fs = require("fs").promises;
 const path = require("path");
 const axios = require("axios");
-const { S3, PutObjectCommand } = require("@aws-sdk/client-s3");
+const {
+  S3,
+  PutObjectCommand,
+  GetObjectCommand,
+} = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 const send = require("../sms/send");
 
 const mediaHandler = async (url, incomingNumber, formattedMessage, msgID) => {
-  //Create new instance of S3 client using Digital Ocean Spaces API (AWS)
-  const s3Client = new S3({
-    endpoint: process.env.SPACES_ENDPOINT,
-    forcePathStyle: true,
-    region: "us-east-1",
-    credentials: {
-      accessKeyId: process.env.SPACES_ACCESS_KEY,
-      secretAccessKey: process.env.SPACES_SECRET_ACCESS_KEY,
-    },
-  });
   //Create a file path and save to fileLocation variable
   //If no text content was sent, only an image, use the msgID parameter from Telnyx message object as the file name
   const fileLocation = path.resolve(
@@ -57,14 +52,32 @@ const mediaHandler = async (url, incomingNumber, formattedMessage, msgID) => {
       },
     };
 
+    //Create new instance of S3 client using Digital Ocean Spaces API (AWS)
+    const s3Client = new S3({
+      endpoint: process.env.SPACES_ENDPOINT,
+      forcePathStyle: true,
+      region: "us-east-1",
+      credentials: {
+        accessKeyId: process.env.SPACES_ACCESS_KEY,
+        secretAccessKey: process.env.SPACES_SECRET_ACCESS_KEY,
+      },
+    });
+
     //Upload file to s3 bucket
     await s3Client.send(new PutObjectCommand(params));
+
+    //Get presigned URL from the file upload request
+    const command = new GetObjectCommand(params);
+    const preSignedUrl = await getSignedUrl(s3Client, command, {
+      expiresIn: 3600,
+    });
 
     //Remove local copy of file
     await fs.unlink(fileLocation);
 
     //Send SMS response of successful upload
-    await send(incomingNumber, "I stashed that file for you.");
+    let successMessage = `I stashed that file for you. Here is a temporary URL for download. \n${preSignedUrl}`;
+    await send(incomingNumber, successMessage);
   } catch (err) {
     console.log("Error", err);
   }
