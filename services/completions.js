8d82bde92946fd0533e90ff7f5382fd152af58cb
Matt <gatorgoat@protonmail.com>
2024-12-29 11:16:16 -0600

    Keep history of conversation and use for ai prompt

--- services/completions.js
+++ services/completions.js
@@ -5,17 +5,17 @@ const openAiApiKey = process.env.OPENAI_API_KEY;
 
 const openai = new OpenAI(openAiApiKey);
 
+let conversationHistory = [];
+
 const completions = async (messageContent, incomingNumber) => {
+  conversationHistory.push({
+    role: `${conversationHistory.length == 0 ? "developer" : "user"}`,
+    content: messageContent,
+  });
+
   //OpenAI API call. Pass received messageContent to the OpenAI messages array with a user role
   const completion = await openai.chat.completions.create({
-    messages: [
-      {
-        role: "system",
-        content:
-          "You are a helpful assistant. Please limit your response to 1000 characters or less.",
-      },
-      { role: "user", content: messageContent },
-    ],
+    messages: [...conversationHistory],
     model: "gpt-3.5-turbo",
   });
   const messagePayload = completion.choices[0].message.content;
